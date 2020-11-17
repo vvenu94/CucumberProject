31cfd1906fdcc97580a4c560483a06c3ae3c8f81
vvenu94 <v.venugopal94@gmail.com>
2020-11-17 18:07:07 +0530

    Change in jvm

--- src/test/java/com/libraries/JVMReport.java
+++ src/test/java/com/libraries/JVMReport.java
@@ -7,19 +7,19 @@ import java.util.List;
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
 
-public  class JVMReport {
+public class JVMReport {
 	public static void jvmReports(String jsonFile) {
-		File directory=new File("C:\\Users\\venugopal\\eclipse-workspace\\BlueAir\\src\\test\\resources\\jvm");
-		Configuration configuration=new Configuration(directory, "Adactin-Automation");
+		File directory = new File("C:\\Users\\venugopal\\eclipse-workspace\\BlueAir\\src\\test\\resources\\jvm");
+		Configuration configuration = new Configuration(directory, "Adactin-Automation");
 		configuration.addClassifications("os", "Windows 10");
 		configuration.addClassifications("browser_Name", "chrome");
 		configuration.addClassifications("browser_Version", "86");
 		configuration.addClassifications("Sprint", "45");
-		List<String> jsonFiles=new ArrayList<>();
+		List<String> jsonFiles = new ArrayList<String>();
 		jsonFiles.add(jsonFile);
-		ReportBuilder reportBuilder=new ReportBuilder(jsonFiles, configuration);
+		ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
 		reportBuilder.generateReports();
-		
+
 	}
 
 }
